461dae9b335ef2dda3a8ce204d583f3256d88bf3
Jinchul park <jinchul112@gmail.com>
2020-08-21 14:48:43 +0200

    up

---
+++ level5/Print_Memory/pm.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    if (c >= 32 && c <= 126)
+    {
+        write(1, &c, 1);
+    }
+    else
+    {
+        write(1, ".", 1);
+    }
+}
+
+void print_hex(int x)
+{
+    char *s = "0123456789abcdef";
+
+    write(1, &s[x / 16], 1);
+    write(1, &s[x % 16], 1);
+}
+
+void print_memory(const void *addr, size_t size)
+{
+    size_t i = 0;
+    size_t j;
+
+    unsigned char const *p = addr;
+    while (i < size)
+    {
+        j = 0;
+        while (j < 16 && i + j < size)
+        {
+            print_hex(*(p + i + j));
+            if (j % 2)
+                write(1, " ", 1);
+            j++;
+        }
+        while (j < 16)
+        {
+            write(1, "  ", 2);
+            if (j % 2)
+                write(1, " ", 1);
+            j++;
+        }
+        j = 0;
+        while (j < 16 && i + j < size)
+        {
+            ft_putchar(*(p + i + j));
+            if (j % 2)
+                write(1, " ", 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i += 16;
+    }
+}
+
+int main(void)
+{
+    int tab[10] = {0, 23, 150, 255,
+                   12, 16, 21, 42};
+
+    print_memory(tab, sizeof(tab));
+    return (0);
+}
---
+++ level5/ft_itoa_base/fita_b.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#define abs(v) (v) < 0 ? -(v) : (v)
+
+void ib(int v, int b, char *res, int *p)
+{
+    char *s = "0123456789ABCDEF";
+
+    if (v >= b || v <= -b)
+        ib(v, b, res, p);
+    res[(*p)++] = s[abs(v % b)];
+}
+
+char *ft_itoa_base(int value, int base)
+{
+    int p = 0;
+    char *res;
+
+    if (((value < 2 || value > 16) || !(res = (char *)malloc(sizeof(char) * 35))))
+        return NULL;
+    if (value < 0 && base == 10)
+        res[p++] = '-';
+    ib(value, base, res, &p);
+    res[p] = '\0';
+    return (res);
+}
