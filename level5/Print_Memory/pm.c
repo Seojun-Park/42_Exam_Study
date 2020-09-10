bab9edfaf8af8c7608c24e32dd9b5e4acdfc9a68
Jinchul park <jinchul112@gmail.com>
2020-09-10 21:51:51 +0200

    up

--- level5/Print_Memory/pm.c
+++ level5/Print_Memory/pm.c
@@ -1,19 +1,17 @@
 #include <unistd.h>
 
-void print_hex(int x)
+void ft_print_hex(int x)
 {
-    char *s = "0123456789abcdef";
+    char *base = "0123456789abcdef";
 
-    write(1, &s[x / 16], 1);
-    write(1, &s[x % 16], 1);
+    write(1, &base[x / 16], 1);
+    write(1, &base[x % 16], 1);
 }
 
 void ft_putchar(char c)
 {
     if (c >= 32 && c <= 126)
-    {
         write(1, &c, 1);
-    }
     else
         write(1, ".", 1);
 }
@@ -22,14 +20,14 @@ void print_memory(const void *addr, size_t size)
 {
     size_t i = 0;
     size_t j;
-
     unsigned char const *p = addr;
+
     while (i < size)
     {
         j = 0;
         while (j < 16 && i + j < size)
         {
-            print_hex(*(p + i + j));
+            ft_print_hex(*(p + i + j));
             if (j % 2)
                 write(1, " ", 1);
             j++;
@@ -45,8 +43,6 @@ void print_memory(const void *addr, size_t size)
         while (j < 16 && i + j < size)
         {
             ft_putchar(*(p + i + j));
-            if (j % 2)
-                write(1, " ", 1);
             j++;
         }
         write(1, "\n", 1);
