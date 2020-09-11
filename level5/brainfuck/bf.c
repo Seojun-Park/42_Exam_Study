533420b28e991f2e16b31cf889965804f8486b6e
Seojun_Park <jinchul112@gmail.com>
2020-09-11 16:52:18 +0200

    up

--- level5/brainfuck/bf.c
+++ level5/brainfuck/bf.c
@@ -6,7 +6,7 @@
 /*   By: jinpark <jinpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 13:16:08 by jinpark           #+#    #+#             */
-/*   Updated: 2020/09/11 14:28:54 by jinpark          ###   ########.fr       */
+/*   Updated: 2020/09/11 15:45:11 by jinpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ void brainfuck(char *str)
 {
     int tab[2048] = {0};
     int *ptr;
-    int cnt;
+    int i = 0;
 
     ptr = tab;
-    cnt = 0;
     while (*str)
     {
         if (*str == '>')
@@ -35,26 +34,26 @@ void brainfuck(char *str)
             write(1, ptr, 1);
         else if (*str == '[' && *ptr == 0)
         {
-            cnt = 1;
-            while (cnt != 0)
+            i = 1;
+            while (i != 0)
             {
                 str++;
                 if (*str == ']')
-                    --cnt;
+                    --i;
                 if (*str == '[')
-                    ++cnt;
+                    ++i;
             }
         }
         else if (*str == ']' && *ptr != 0)
         {
-            cnt = 1;
-            while (cnt != 0)
+            i = 1;
+            while (i != 0)
             {
                 str--;
                 if (*str == '[')
-                    --cnt;
+                    --i;
                 if (*str == ']')
-                    ++cnt;
+                    ++i;
             }
         }
         str++;
