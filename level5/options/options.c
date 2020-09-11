b56f779e8ea96bad96edb2a6260415aa2e218ad9
Seojun_Park <jinchul112@gmail.com>
2020-09-11 13:49:56 +0200

    up

--- level5/options/options.c
+++ level5/options/options.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   options.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maparmar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jinpark <jinpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/24 19:32:40 by maparmar          #+#    #+#             */
-/*   Updated: 2019/03/24 19:32:46 by maparmar         ###   ########.fr       */
+/*   Updated: 2020/09/11 13:43:27 by jinpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 int main(int ac, char **av)
 {
 	int i = 1;
-	int  t[32] = {0}; 
-	int j ;
+	int t[32] = {0};
+	int j;
 
-	if(ac == 1)
+	if (ac == 1)
 	{
-		write(1,"options: abcdefghijklmnopqrstuvwxyz\n",36);
+		write(1, "options: abcdefghijklmnopqrstuvwxyz\n", 36);
 		return 0;
 	}
 	i = 1;
 	while (i < ac)
 	{
 		j = 1;
-		if(av[i][0] == '-')
+		if (av[i][0] == '-')
 		{
-			while(av[i][j] && av[i][j] >= 'a'  && av[i][j] <= 'z')
+			while (av[i][j] && av[i][j] >= 'a' && av[i][j] <= 'z')
 			{
-				if(av[i][j] == 'h')
+				if (av[i][j] == 'h')
 				{
-					write(1,"options: abcdefghijklmnopqrstuvwxyz\n",36);
+					write(1, "options: abcdefghijklmnopqrstuvwxyz\n", 36);
 					return 0;
 				}
 
@@ -43,7 +43,7 @@ int main(int ac, char **av)
 
 			if (av[i][j])
 			{
-				write(1,"Invalid Option\n",15);
+				write(1, "Invalid Option\n", 15);
 				return 0;
 			}
 			j++;
@@ -51,16 +51,15 @@ int main(int ac, char **av)
 		i++;
 	}
 	i = 0;
-		while (i < 32)
-		{
+	while (i < 32)
+	{
 		t[i] = '0' + t[i];
-		write(1,&t[i++],1);
-			if(i == 32)
-				write(1,"\n",1);
-			else if(i % 8 == 0)
-				write(1," ",1);
-
-		}
+		write(1, &t[i++], 1);
+		if (i == 32)
+			write(1, "\n", 1);
+		else if (i % 8 == 0)
+			write(1, " ", 1);
+	}
 
 	return 0;
 }
